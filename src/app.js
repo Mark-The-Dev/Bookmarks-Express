48ead2d69bd2c2f657c77348e1bf4d171f2da8fa
Malik DeJean <mdejean@MacBook-Pro.local>
2020-09-18 11:41:47 -0700

    bearer authorization

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const logger = require('./logger')
 const bookmarksRouter = require('./bookmarks/bookmarks-router');
 
 const app = express();
@@ -16,6 +17,16 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next();
+});
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
