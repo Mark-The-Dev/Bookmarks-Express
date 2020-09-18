0dab0ea8704072f9ede556339a41ba95410eb02f
Malik DeJean <mdejean@MacBook-Pro.local>
2020-09-18 11:31:36 -0700

    get :id and delete

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -33,7 +33,7 @@ bookmarksRouter
         .status(400)
         .send('Invalid data');
     }
-    if (!rating || rating < 1 || rating > 5 ) {
+    if (!rating || rating < 1 || rating > 5 || typeof rating !== 'number') {
       logger.error(`rating is required`);
       return res
         .status(400)
@@ -58,6 +58,44 @@ bookmarksRouter
       .location(`http://localhost:8000/bookmarks/${id}`)
       .json({ newBookmark, message: 'it worked!' });
     // .json(newBookmark)
+  });
+
+bookmarksRouter
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+
+    const { id } = req.params
+    const bookmark = bookmarks.find(b => b.id === id);
+
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found`);
+      return res
+        .status(404)
+        .send('Bookmark not found');
+    }
+
+    res.json(bookmark);
+  })
+  .delete((req, res) => {
+
+    const { id } = req.params;
+
+    const bookmarkIndex = bookmarks.findIndex(b => b.id === id);
+
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Bookmark not found');
+    }
+
+    bookmarks.splice(bookmarkIndex, 1);
+
+    logger.info(`Bookmark with id ${id} deleted.`);
+
+    res
+      .status(204)
+      .end();
   });
 
 
